414d9a6f4752ff6c57245f0fd6c9e783d0ad72e1
Gonzalo <gonzalo.fernandez.ripa@gmail.com>
2022-03-18 10:42:19 +0100

    GetSet

--- Libreria/src/clases/Libro.java
+++ Libreria/src/clases/Libro.java
@@ -6,5 +6,24 @@ public class Libro {
 	private String isbn;
 	private String nombre;
 	private LocalDate fechapub;
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public LocalDate getFechapub() {
+		return fechapub;
+	}
+	public void setFechapub(LocalDate fechapub) {
+		this.fechapub = fechapub;
+	}
+	
 	
 }
