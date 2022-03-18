8cd53a8caaf1c5e12cfd8f201b0bd43dbe31eaca
HaizeaF <haizea.franco@gmail.com>
2022-03-18 09:55:10 +0100

    atributosEstanteria

--- Libreria/src/clases/Escritor.java
+++ Libreria/src/clases/Escritor.java
@@ -1,5 +1,10 @@
 package clases;
 
-public class Escritor {
+import java.time.LocalDate;
 
+public class Escritor {
+	private String nombre;
+	private String apellido;
+	private String dni;
+	private LocalDate fechaNac;
 }
