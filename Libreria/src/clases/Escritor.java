f2015d95034f54d934cf32709d04f63e02d41b4c
HaizeaF <haizea.franco@gmail.com>
2022-03-18 10:06:27 +0100

    constructores, getters y setters de escritor

--- Libreria/src/clases/Escritor.java
+++ Libreria/src/clases/Escritor.java
@@ -7,4 +7,40 @@ public class Escritor {
 	private String apellido;
 	private String dni;
 	private LocalDate fechaNac;
+	public Escritor(String nombre, String apellido, String dni, LocalDate fechaNac) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+		this.fechaNac = fechaNac;
+	}
+	public Escritor() {
+		super();
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public LocalDate getFechaNac() {
+		return fechaNac;
+	}
+	public void setFechaNac(LocalDate fechaNac) {
+		this.fechaNac = fechaNac;
+	}
+	
+	
 }
